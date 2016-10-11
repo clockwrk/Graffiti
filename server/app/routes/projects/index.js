1fb7d3c7f8a5765cab7160c2e04eeea82a8ec517
Kate Humphrey <katehumphrey@Kates-MacBook-Pro-2.local>
2016-10-11 16:41:27 -0400

    more notes

--- server/app/routes/projects/index.js
+++ server/app/routes/projects/index.js
@@ -37,23 +37,23 @@ router.post('/', (req, res, next) => {
     	.then(completedAssociations => res.end()) //figure out what comes back and what we want to send in this moment
     	.catch(next)
     
-    //alternative that I think will work too!
-    Project.create({
-        texts:[req.body.texts],
-        drawing: req.body.drawing,
-        images: [req.body.images],
-        location: req.body.location,
-        user: req.body.user
-    })
-    .then(createdProject => res.json(creationProject))
-    .catch(next)
+    // //alternative that I think will work too!
+    // Project.create({
+    //     texts:[req.body.texts],
+    //     drawing: req.body.drawing,
+    //     images: [req.body.images],
+    //     location: req.body.location,
+    //     user: req.body.user
+    // })
+    // .then(createdProject => res.json(creationProject))
+    // .catch(next)
 });
 
 
 router.delete('/:id', (req, res, next) => {
     Project.destroy({
         where: { id: req.params.id }
-    })
+    }) //I think this should work and destroy the associations first, but we might have to find the associations in a hook and destroy them, then the project...
     .then(linesDestroyed => res.sendStatus(204))
     .catch(next)
 })
